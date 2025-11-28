2173596e560c836b4e8a225ee78f73ce8c7b835e
rdzinhoo941 <dofetal94@gmail.com>
2025-11-28 16:27:26 +0100

    Import additional modules for strategy and portfolio

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ from scipy.stats import norm  # prêt pour extensions VaR
 from sklearn.linear_model import LinearRegression
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.stattools import adfuller
+
+from strategy import page_strategy  # strategy.py
+from portfolio import page_portfolio  # portfolio.py*
+from home import page_home  # home.py
+from chart import price_chart
+from data import get_price_series
