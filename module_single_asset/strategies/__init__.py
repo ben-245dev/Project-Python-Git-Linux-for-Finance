3ca8ef4a40e3a68104ff534d3b5ef172f4f86cb0
ben-245dev <decompsbenoit@gmail.com>
2025-12-26 12:02:38 +0100

    change main to new with qunt B modules

--- module_single_asset/strategies/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import TradingStrategy
-from .momentum import MomentumStrategy
-from .bollinger import BollingerStrategy
-from .meanreversion import MeanReversionStrategy
-from .breakout import BreakoutStrategy
